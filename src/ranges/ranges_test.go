a501941dae914e663ab000b6481f2ed3495aeb23
John Floren <john@jfloren.net>
2012-11-01 17:15:39 -0700

    fixed up a test because Fritz was sad

--- src/ranges/ranges_test.go
+++ src/ranges/ranges_test.go
@@ -2,10 +2,19 @@ package ranges
 
 import "testing"
 import "fmt"
-//BUG(floren): this is not a good test
+
 func TestSplitRange(t *testing.T) {
 	r, _ := NewRange("kn", 1, 520)
 
-	res, _ := r.SplitRange("kn[1-20,5,10]")
-	fmt.Println(res)
+	expected := []string{ "kn1", "kn2", "kn3", "kn100" }
+	input := "kn[1-3,100]"
+
+	res, _ := r.SplitRange(input)
+
+	es := fmt.Sprintf("%v", expected)
+	rs := fmt.Sprintf("%v", res)
+
+	if es != rs {
+		t.Fatal("SplitRange returned: ", res, ", expected: ", expected)
+	}
 }
