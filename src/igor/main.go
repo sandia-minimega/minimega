98009af67b786a5468edebb30fe1e23540af3b3b
John Floren <john@jfloren.net>
2012-11-06 11:40:21 -0800

    removed non-functional add command

--- src/igor/main.go
+++ src/igor/main.go
@@ -92,7 +92,6 @@ func (c *Command) Runnable() bool {
 // Commands lists the available commands and help topics.
 // The order here is the order in which they are printed by 'go help'.
 var commands = []*Command{
-	cmdAdd,
 	cmdDel,
 	cmdShow,
 	cmdSub,
