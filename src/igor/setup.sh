3ee828ffd9b72d9bd92da2e8b250eac1205ee0c9
John Floren <john@jfloren.net>
2013-05-21 16:14:54 -0700

    improved install

--- src/igor/setup.sh
+++ src/igor/setup.sh
@@ -1,11 +1,19 @@
-#!/bin/sh -e
+#!/bin/bash -e
 
 echo This script will run the following commands to set up igor. Press Ctrl-C to cancel, or hit Enter to continue:
+echo useradd igor
+echo cp ../../bin/igor /usr/local/bin/
+echo chown igor:igor /usr/local/bin/igor
+echo chmod +s /usr/local/bin/igor
 echo mkdir $1/igor
 echo mkdir $1/pxelinux.cfg/igor
 echo touch $1/igor/reservations.json
 echo cp sampleconfig.json /etc/igor.conf
 read
+useradd igor
+cp ../../bin/igor /usr/local/bin/
+chown igor:igor /usr/local/bin/igor
+chmod +s /usr/local/bin/igor
 mkdir $1/igor
 mkdir $1/pxelinux.cfg/igor
 touch $1/igor/reservations.json
