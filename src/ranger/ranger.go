cb551a792ab6d37538054c8f8e74259fb04c086c
fritz <fritz@bieberfever>
2013-08-28 14:18:00 -0600

    make sure licensing is correct

--- src/ranger/ranger.go
+++ src/ranger/ranger.go
@@ -1,3 +1,7 @@
+// Copyright (2013) Sandia Corporation.
+// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// the U.S. Government retains certain rights in this software.
+
 package main
 
 import (
