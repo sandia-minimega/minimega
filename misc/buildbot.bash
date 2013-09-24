ab48fd139e9ce1e19fdc53f1e47856c927fb0ead
fritz <fritz@bieberfever>
2013-09-24 11:43:43 -0600

    update buildbot

--- misc/buildbot.bash
+++ misc/buildbot.bash
@@ -4,13 +4,13 @@
 
 export PATH=${PATH}:/opt/go/bin
 
-cd /home/fritz/buildbot/mega
+cd /home/fritz/buildbot/minimega
 git pull
 ./clean.bash
 ./build.bash > build.log
 cd /home/fritz/buildbot
 DATE=`date +%Y-%m-%d`
-mkdir mega-$DATE && cd mega-$DATE
+mkdir minimega-$DATE && cd minimega-$DATE
 mkdir bin
 mkdir src
 mkdir misc
@@ -18,10 +18,10 @@ cp ../mega/bin/* bin
 cp -r ../mega/src/* src
 cp -r ../mega/misc/* misc
 cd ..
-tar cjf mega-${DATE}.tar.bz2 mega-$DATE
+tar cjf minimega-${DATE}.tar.bz2 minimega-$DATE
 rm /home/fritz/web/minimega.org/nightly/*
-cp mega-${DATE}.tar.bz2 /home/fritz/web/minimega.org/nightly
-cp mega/build.log /home/fritz/web/minimega.org/nightly/mega-${DATE}.log
-ln -s /home/fritz/web/minimega.org/nightly/mega-${DATE}.tar.bz2 /home/fritz/web/minimega.org/nightly/latest.tar.bz2
-rm mega-${DATE}.tar.bz2
-rm -rf mega-${DATE}
+cp minimega-${DATE}.tar.bz2 /home/fritz/web/minimega.org/nightly
+cp minimega/build.log /home/fritz/web/minimega.org/nightly/minimega-${DATE}.log
+ln -s /home/fritz/web/minimega.org/nightly/minimega-${DATE}.tar.bz2 /home/fritz/web/minimega.org/nightly/minimega-latest.tar.bz2
+rm minimega-${DATE}.tar.bz2
+rm -rf minimega-${DATE}
