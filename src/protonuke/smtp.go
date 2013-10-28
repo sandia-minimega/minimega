85d9ae2c4208f22bf56d8c35d74afa6ebeac1ae5
John Floren <john@jfloren.net>
2013-10-28 16:34:47 -0700

    more commands, not a lot of error checking yet

--- src/protonuke/smtp.go
+++ src/protonuke/smtp.go
@@ -22,9 +22,14 @@ func smtpClient() {
 }
 
 type SMTPClientSession struct {
-	conn  net.Conn
-	bufin *bufio.Reader
-	state int
+	conn      net.Conn
+	bufin     *bufio.Reader
+	bufout    *bufio.Writer
+	state     int
+	response  string
+	mail_from string
+	rcpt_to   []string
+	data      string
 }
 
 const (
@@ -37,11 +42,14 @@ const (
 var (
 	timeout  = time.Duration(100)
 	max_size = 131072
+	myFQDN   = "protonuke.local"
 )
 
 func NewSMTPClientSession(c net.Conn) *SMTPClientSession {
 	ret := &SMTPClientSession{conn: c, state: INIT}
 	ret.bufin = bufio.NewReader(c)
+	ret.bufout = bufio.NewWriter(c)
+	ret.rcpt_to = []string{}
 	return ret
 }
 
@@ -76,9 +84,55 @@ func (s *SMTPClientSession) Handler() {
 	for {
 		switch s.state {
 		case INIT:
-
+			s.addResponse("220 protonuke: Less for outcasts, more for weirdos.")
+			s.state = COMMANDS
+		case COMMANDS:
+			input, err := s.readSmtp()
+			input = strings.Trim(input, "\r\n")
+			cmd := strings.ToUpper(input)
+			if err != nil {
+				log.Debugln(err)
+				continue
+			}
+			switch {
+			case strings.HasPrefix(cmd, "HELO"):
+				s.addResponse("250 You're all so great and we're gonna keep you listening all day")
+			case strings.HasPrefix(cmd, "EHLO"):
+				r := "250-" + myFQDN + " Serving mail almost makes you wish for a nuclear winter\r\n"
+				r += "250-8BITMIME\r\n250-STARTTLS\r\n250 HELP"
+				s.addResponse(r)
+			case strings.HasPrefix(cmd, "MAIL FROM:"):
+				if len(input) > 10 {
+					s.mail_from = input[10:]
+				}
+				s.addResponse("250 Ok")
+			case strings.HasPrefix(cmd, "RCPT TO:"):
+				if len(input) > 8 {
+					s.rcpt_to = append(s.rcpt_to, input[8:])
+				}
+				s.addResponse("250 Ok")
+			case strings.HasPrefix(cmd, "DATA"):
+				s.addResponse("354 End data with <CR><LF>.<CR><LF>")
+				s.state = DATA
+			}
+		case DATA:
+			input, err := s.readSmtp()
+			if err != nil {
+				log.Debugln(err)
+			}
+			s.data = input
+			s.state = COMMANDS
+			log.Debugln("Got email message:")
+			log.Debugln(s)
 		}
+		size, _ := s.bufout.WriteString(s.response)
+		s.bufout.Flush()
+		s.response = s.response[size:]
 	}
+}
+
+func (s *SMTPClientSession) addResponse(r string) {
+	s.response += r + "\r\n"
 }
 
 // Read a line from the client.
