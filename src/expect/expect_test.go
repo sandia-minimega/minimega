1725d11605f047067f8168142a6d9ea51920a23f
John Floren <john@jfloren.net>
2014-01-16 15:39:23 -0800

    updated expect test

--- src/expect/expect_test.go
+++ src/expect/expect_test.go
@@ -7,7 +7,11 @@ import "bytes"
 func TestExpect(t *testing.T) {
 	input := []byte("Hello\r\nThis is a test\r\nlogin: ")
 	e := NewExpecter(bytes.NewReader(input))
-	res, err := e.Expect("login: ")
+	res, err := e.Expect("This ")
+	if err != nil {
+		t.Fatal("Expect returned: ", res, ", error: ", err)
+	}
+	res, err = e.Expect("login: ")
 	if err != nil {
 		t.Fatal("Expect returned: ", res, ", error: ", err)
 	}
