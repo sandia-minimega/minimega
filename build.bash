8bfaab3ca8d7dd4d654e036042b711d01699d937
fritz <fritz@bieberfever>
2014-03-29 19:47:27 -0600

    add race detector editions of the linux binaries to the build script

--- build.bash
+++ build.bash
@@ -15,6 +15,22 @@ var (
 )
 " > $SCRIPT_DIR/src/version/version.go
 
+# build packages with race detection
+echo "BUILD RACE PACKAGES (linux)"
+echo "protonuke"
+go install -race protonuke
+cp $SCRIPT_DIR/bin/protonuke $SCRIPT_DIR/bin/protonuke_race
+echo "minimega"
+go install -race minimega
+cp $SCRIPT_DIR/bin/minimega $SCRIPT_DIR/bin/minimega_race
+echo "miniccc"
+go install -race miniccc
+cp $SCRIPT_DIR/bin/miniccc $SCRIPT_DIR/bin/miniccc_race
+echo "vmbetter"
+go install -race vmbetter
+cp $SCRIPT_DIR/bin/vmbetter $SCRIPT_DIR/bin/vmbetter_race
+echo
+
 # build packages
 echo "BUILD PACKAGES (linux)"
 for i in `ls $SCRIPT_DIR/src`
@@ -24,6 +40,7 @@ do
 done
 echo
 
+# build windows packages
 echo "BUILD PACKAGES (windows)"
 echo "protonuke"
 GOOS=windows go install protonuke
