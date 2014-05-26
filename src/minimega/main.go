b1f239437c1e5859aa5a525866ac46e564e075f9
fritz <fritz@bieberfever>
2014-05-26 11:08:11 -0600

    documentation work, update installing, usage markdown, style, and copyright notice

--- src/minimega/main.go
+++ src/minimega/main.go
@@ -45,7 +45,7 @@ var (
 	panicOnQuit  bool
 )
 
-var banner string = `minimega, Copyright (2013) Sandia Corporation. 
+var banner string = `minimega, Copyright (2014) Sandia Corporation. 
 Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation, 
 the U.S. Government retains certain rights in this software.
 `
