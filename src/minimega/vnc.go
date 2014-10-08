911dfc8b2ccf9cddb374ffc704a75d445b8d7faa
John Floren <john@jfloren.net>
2014-10-08 13:27:39 -0700

    slightly fleshed-out vnc.go

--- src/minimega/vnc.go
+++ src/minimega/vnc.go
@@ -3,6 +3,23 @@ package main
 import ()
 
 func cliVNC(c cliCommand) cliResponse {
+	switch len(c.Args) {
+	case 4: // [record|playback] <host> <vm> <file>
+		if c.Args[0] != "record" && c.Args[0] != "playback" {
+			return cliResponse{
+				Error: "malformed command",
+			}
+		}
+		/*
+			host := c.Args[1]
+			vm := c.Args[2]
+			filename := c.Args[3]
+		*/
+	default:
+		return cliResponse{
+			Error: "malformed command",
+		}
+	}
 	return cliResponse{
 		Error: "not yet implemented",
 	}
