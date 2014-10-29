41bccfa517e73fee03e7c07bab4a00c44855c608
Jon Crussell <jcrusse@sandia.gov>
2014-10-29 13:32:54 -0700

    Added echo server.
    Reads bytes from serial and writes them back. Used for testing whether
    goserial is actually working under windows.

---
+++ src/goserial/echo/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"goserial"
+	"log"
+	"os"
+)
+
+var (
+	baud = flag.Int("baud", 115200, "set the baud rate")
+)
+
+func usage() {
+	fmt.Printf("USAGE: %s [OPTION]... PORT", os.Args[0])
+	flag.PrintDefaults()
+}
+
+func main() {
+	flag.Usage = usage
+	flag.Parse()
+
+	if flag.NArg() != 1 {
+		usage()
+		os.Exit(1)
+	}
+
+	c := &serial.Config{Name: flag.Arg(0), Baud: *baud}
+
+	s, err := serial.OpenPort(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var n int
+	buf := make([]byte, 128)
+	for {
+		n, err = s.Read(buf)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, err := s.Write(buf[:n])
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
