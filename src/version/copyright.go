4774205752bf3218441fe44cb2b2e17d585ee142
Jon Crussell <jcrusse@sandia.gov>
2015-01-12 16:09:36 -0800

    gomacro: removing.
    Removing now-unused package gomacro. Code exists in an external repo so
    we can readd it later, if needed.

--- src/version/copyright.go
+++ src/version/copyright.go
@@ -2,8 +2,8 @@ package version
 
 var (
 	Copyright = `
-minimega, Copyright (2013) Sandia Corporation. 
-Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation, 
+minimega, Copyright (2013) Sandia Corporation.
+Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
 the U.S. Government retains certain rights in this software.
 
 minimega comes with ABSOLUTELY NO WARRANTY. This is free software, and you are
@@ -13,7 +13,5 @@ for details.
 websocket and ssh packages Copyright The Go Authors. All rights reserved.
 
 noVNC Copyright 2011 Joel Martin <github@martintribe.org>
-
-gomacro Copyright 2014 David Fritz <david.jakob.fritz@gmail.com>
 `
 )
