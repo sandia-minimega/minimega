2c83e2029dbecc966965d9f7d120eda4a403ba68
John Floren <john@jfloren.net>
2015-01-30 13:39:12 -0800

    initial draft of building debian packages

---
+++ packaging/debian/build-package.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -e
+# You *must* run this from the packaging/debian directory.
+
+mm_root=`pwd`/../../
+dest_opt=./minimega/opt/minimega/
+
+echo BUILDING MINIMEGA...
+(cd $mm_root && ./build.bash)
+echo DONE BUILDING
+
+echo COPYING FILES...
+mkdir -p ./minimega/opt/minimega/
+cp -r $mm_root/bin $dest_opt
+cp -r $mm_root/misc $dest_opt
+cp -r $mm_root/doc $dest_opt
+
+mkdir -p ./minimega/usr/share/doc/minimega
+cp $mm_root/LICENSES/* ./minimega/usr/share/doc/minimega/
+echo COPIED FILES
