95e3acf4e51a2f4996ced4f2a6489b5aee5132b5
David Fritz <djfritz@sandia.gov>
2015-02-17 18:04:06 -0800

    ron: put virtio access into overlapped mode for windows. windows is such junk.

--- src/ron/client.go
+++ src/ron/client.go
@@ -36,25 +36,6 @@ func (c *Client) dial(parent string, port int) error {
 	return nil
 }
 
-func (c *Client) dialSerial(path string) error {
-	log.Debug("ron dialSerial; %v", path)
-
-	conn, err := os.OpenFile(path, os.O_RDWR, 0666)
-	if err != nil {
-		return err
-	}
-
-	c.conn = conn
-
-	go c.handler()
-	go c.mux()
-	go c.periodic()
-	go c.commandHandler()
-	c.heartbeat()
-
-	return nil
-}
-
 func (c *Client) Respond(r *Response) {
 	log.Debug("ron Respond: %v", r.ID)
 
