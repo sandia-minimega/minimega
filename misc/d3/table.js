05bd6250c1d95fabdff21b95c33a423099499f52
Brian W <bj.wrigh@gmail.com>
2015-03-16 20:24:51 -0600

    missed a file

---
+++ misc/d3/table.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    var table = $('#example').DataTable( {
+        "scrollY": "200px",
+        "paging": false
+    } );
+ 
+    $('a.toggle-vis').on( 'click', function (e) {
+        e.preventDefault();
+ 
+        // Get the column API object
+        var column = table.column( $(this).attr('data-column') );
+ 
+        // Toggle the visibility
+        column.visible( ! column.visible() );
+    } );
+} );
