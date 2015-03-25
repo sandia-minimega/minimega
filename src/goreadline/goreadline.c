b46d492ec17b6a7b0622195d81bb0c65c55f7138
fritz <djfritz@sandia.gov>
2015-03-25 13:25:18 -0600

    add Sandia copyright notice to source files that are missing it.
    fixes #122

--- src/goreadline/goreadline.c
+++ src/goreadline/goreadline.c
@@ -1,3 +1,9 @@
+/* 
+ * Copyright (2015) Sandia Corporation.
+ * Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+ * the U.S. Government retains certain rights in this software.
+*/
+
 #include "_cgo_export.h"
 #include "readline/readline.h"
 #include <stdio.h>
