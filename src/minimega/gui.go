f5e6885ebaa6cc13aa9787cd6c56cc5187faeb6a
John Floren <john@jfloren.net>
2015-03-26 14:18:54 -0700

    minimega: broke gui's HTMLFRAME and D3MAP content out into the new templates directory.

--- src/minimega/gui.go
+++ src/minimega/gui.go
@@ -8,6 +8,7 @@ package main
 import (
 	"fmt"
 	"html"
+	"io/ioutil"
 	"minicli"
 	log "minilog"
 	"net/http"
@@ -19,142 +20,14 @@ import (
 const (
 	GUI_PORT              = 9001
 	defaultWebroot string = "misc/web"
-	HTMLFRAME             = `<!DOCTYPE html>
-				<head><title>Minimega GUI</title>
-				<link rel="stylesheet" type="text/css" href="/gui/d3/nav.css">
-				<link rel="stylesheet" type="text/css" href="/gui/d3/jquery.dataTables.css">
-				<script type="text/javascript" language="javascript" src="/gui/d3/jquery-1.11.1.min.js"></script>
-				<script type="text/javascript" language="javascript" src="/gui/d3/jquery.dataTables.min.js"></script>
-				%s
-				</head>
-				<body>
-				<nav><ul>
-				  <!--<li><a href="/gui/vnc">Host List</a></li>-->
-				  <li><a href="/gui/all">All VMs</a></li>
-				  <li><a href="/gui/tile">VM Tile</a></li>
-				  <li><a href="/gui/stats">Host Stats</a></li>
-				  <li><a href="/gui/errors">VM Errors</a></li>
-				  <li><a href="/gui/state">State of Health</a></li>
-				  <li><a href="/gui/map">VM Map</a></li>
-				 <!-- <li><a href="/gui/graph">Graph</a></li>
-				  <li><a href="/gui/terminal/terminal.html">Terminal(concept)</a></li>-->
-				</ul></nav>
-				%s
-				</body></html>`
-
-	D3MAP = `
-		<div id="container"></div>
-		<script src="/gui/d3/d3.min.js"></script>
-		<script src="/gui/d3/topojson.v1.min.js"></script>
-		<script>
-		d3.select(window).on("resize", throttle);
-		var zoom = d3.behavior.zoom().scaleExtent([1, 9]).on("zoom", move);
-		var width = document.getElementById('container').offsetWidth;
-		var height = width / 2;
-		var topo,projection,path,svg,g;
-		var graticule = d3.geo.graticule();
-		var tooltip = d3.select("#container").append("div").attr("class", "tooltip hidden");
-		setup(width,height);
-		function setup(width,height){
-		  projection = d3.geo.mercator().translate([(width/2), (height/2)]).scale( width / 2 / Math.PI);
-		  path = d3.geo.path().projection(projection);
-		  svg = d3.select("#container").append("svg")
-		      .attr("width", width)
-		      .attr("height", height)
-		      .call(zoom)
-		      .on("click", click)
-		      .append("g");
-		  g = svg.append("g").on("click", click);
-		}
-		d3.json("/gui/d3/world-topo-min.json", function(error, world) {
-		  var countries = topojson.feature(world, world.objects.countries).features;
-		  topo = countries;
-		  draw(topo);
-		});
-		function draw(topo) {
-		  svg.append("path")
-		  svg.append("path").datum(graticule).attr("class", "graticule").attr("d", path);
-		  g.append("path")
-		   .datum({type: "LineString", coordinates: [[-180, 0], [-90, 0], [0, 0], [90, 0], [180, 0]]})
-		   .attr("class", "equator")
-		   .attr("d", path);
-		  var country = g.selectAll(".country").data(topo);
-		  country.enter().insert("path")
-		      .attr("class", "country")
-		      .attr("d", path)
-		      .attr("id", function(d,i) { return d.id; })
-		      .attr("title", function(d,i) { return d.properties.name; })
-		      .style("fill", function(d, i) { return d.properties.color; });
-		  //offsets for tooltips
-		  var offsetL = document.getElementById('container').offsetLeft+20;
-		  var offsetT = document.getElementById('container').offsetTop+10;
-		  //tooltips
-		  country.on("mousemove", function(d,i) {
-		      var mouse = d3.mouse(svg.node()).map( function(d) { return parseInt(d); } );
-		      tooltip.classed("hidden", false)
-			     .attr("style", "left:"+(mouse[0]+offsetL)+"px;top:"+(mouse[1]+offsetT)+"px")
-			     .html(d.properties.name);
-		      })
-		      .on("mouseout",  function(d,i) {
-			tooltip.classed("hidden", true);
-		      });
-		%s
-		}
-		function redraw() {
-		  width = document.getElementById('container').offsetWidth;
-		  height = width / 2;
-		  d3.select('svg').remove();
-		  setup(width,height);
-		  draw(topo);
-		}
-		function move() {
-		  var t = d3.event.translate;
-		  var s = d3.event.scale;
-		  zscale = s;
-		  var h = height/4;
-		  t[0] = Math.min(
-		    (width/height)  * (s - 1),
-		    Math.max( width * (1 - s), t[0] )
-		  );
-		  t[1] = Math.min(
-		    h * (s - 1) + h * s,
-		    Math.max(height  * (1 - s) - h * s, t[1])
-		  );
-		  zoom.translate(t);
-		  g.attr("transform", "translate(" + t + ")scale(" + s + ")");
-		  //adjust the country hover stroke width based on zoom level
-		  d3.selectAll(".country").style("stroke-width", 1.5 / s);
-		}
-		var throttleTimer;
-		function throttle() {
-		  window.clearTimeout(throttleTimer);
-		    throttleTimer = window.setTimeout(function() {
-		      redraw();
-		    }, 200);
-		}
-		//geo translation on mouse click in map
-		function click() {
-		  var latlon = projection.invert(d3.mouse(this));
-		  console.log(latlon);
-		}
-		//function to add points and text to the map (used in plotting capitals)
-		function addpoint(lat,lon,text) {
-		  var gpoint = g.append("g").attr("class", "gpoint").attr("xlink:href","http://www.sandia.gov");
-		  var x = projection([lat,lon])[0];
-		  var y = projection([lat,lon])[1];
-		  gpoint.append("svg:circle").attr("cx", x).attr("cy", y).attr("class","point").attr("r", 1.5);
-		  if(text.length>0){    //conditional in case a point has no associated text
-		    gpoint.append("text").attr("x", x+2).attr("y", y+2).attr("class","text").text(text);
-		  }
-		}
-		</script>
-		`
 )
 
 var (
 	guiRunning bool
 	webroot    string = defaultWebroot
 	server     *http.Server
+	HTMLFRAME  string
+	D3MAP      string
 )
 
 var guiCLIHandlers = []minicli.Handler{
@@ -220,6 +93,25 @@ func cliGUI(c *minicli.Command) *minicli.Response {
 	return resp
 }
 
+func initTemplates() error {
+	tmplDir := filepath.Join(webroot, "templates")
+
+	// Read in the HTML frame
+	hf, err := ioutil.ReadFile(filepath.Join(tmplDir, "htmlframe"))
+	if err != nil {
+		return err
+	}
+	HTMLFRAME = string(hf)
+
+	d3m, err := ioutil.ReadFile(filepath.Join(tmplDir, "d3map"))
+	if err != nil {
+		return err
+	}
+	D3MAP = string(d3m)
+
+	return nil
+}
+
 func guiStart(port int) {
 
 	//Look at me! I self-discovered myself!
@@ -242,6 +134,12 @@ func guiStart(port int) {
 	//}
 
 	guiRunning = true
+
+	// re-initialize templates
+	err := initTemplates()
+	if err != nil {
+		log.Error("guiStart: couldn't initalize templates: %v", err)
+	}
 
 	mux := http.NewServeMux()
 	mux.Handle("/gui/novnc/", http.StripPrefix("/gui/novnc/", http.FileServer(http.Dir(filepath.Join(webroot, "novnc")))))
