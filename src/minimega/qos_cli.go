706ccd0cb17dbac68a4d6cc1d47669735ed027c6
cdshann <cdshann@sandia.gov>
2016-03-01 14:28:37 -0800

    added HelpLongs to qos commands and updated the interface to remove qos parameters to be consistent with other minimega commands

--- src/minimega/qos_cli.go
+++ src/minimega/qos_cli.go
@@ -4,28 +4,101 @@ import (
 	"fmt"
 	"minicli"
 	"strconv"
-	"time"
 	"strings"
+	"time"
 )
 
 var qosCLIHandlers = []minicli.Handler{
 	{
 		HelpShort: "add qos constraints to an interface",
-		HelpLong:  "add qos constraints to an interface",
+		HelpLong: `
+Add quality-of-service (qos) constraints on mega interfaces to
+simulate network impairments. Qos constrains cannot be stacked, and must
+be specified explicitly 
+
+Qos constraints include:
+
+- loss		: packets will be randomly dropped with a specified probability
+- delay		: delay packets for configured unit of time
+
+Examples:
+
+	Randomly drop packets on mega_tap1 with probably 0.25%
+	qos add mega_tap1 loss 0.25
+
+	Add a 100ms delay to every packet on the mega_tap1 interface
+	qos add mega_tap1 delay 100ms`,
 		Patterns: []string{
 			"qos <add,> <interface> <loss,> <percent>",
 			"qos <add,> <interface> <delay,> <duration>",
-			"qos <add,> <interface> <loss,> <percent> <delay,> <duration>",
-			"qos <remove,> <interface>",
 		}, Call: wrapSimpleCLI(cliQos),
 	},
 	{
 		HelpShort: "list qos constraints on all interfaces",
-		HelpLong:  "list qos constraints on all interfaces",
+		HelpLong: `
+List quality-of-service constraints on all mega interfaces in tabular form.
+Columns returned by qos list include:
+
+- host		: the host the the VM is running on
+- bridge	: bridge name
+- tap		: tap name
+- loss		: probability of dropping packets
+- delay		: packet delay in milliseconds`,
 		Patterns: []string{
 			"qos list",
 		}, Call: wrapSimpleCLI(cliQosList),
 	},
+	{
+		HelpShort: "clear qos constraints on an interface",
+		HelpLong: `
+Remove quality-of-service constraints on a mega interface.
+
+Example:
+
+	Remove all qos constraints from mega_tap1
+	clear qos mega_tap1`,
+		Patterns: []string{
+			"clear qos [interface]",
+		}, Call: wrapSimpleCLI(cliQosClear),
+	},
+}
+
+func cliQosClear(c *minicli.Command) *minicli.Response {
+	resp := &minicli.Response{Host: hostname}
+
+	tap := c.StringArgs["interface"]
+
+	bridgeLock.Lock()
+	defer bridgeLock.Unlock()
+
+	if tap == "all" {
+		qosRemoveAll()
+	} else {
+		// Get *Tap
+		b, err := getBridgeFromTap(tap)
+		if err != nil {
+			resp.Error = err.Error()
+			return resp
+		}
+
+		t, ok := b.Taps[tap]
+		if !ok {
+			resp.Error = fmt.Sprintf("qosCmd: tap %s not found", tap)
+			return resp
+		}
+
+		// Only remove qos from taps which had previous constraints
+		if t.qos == nil {
+			return resp
+		} else {
+			t.qos = nil
+		}
+		err = t.qosCmd()
+		if err != nil {
+			resp.Error = err.Error()
+		}
+	}
+	return resp
 }
 
 func cliQosList(c *minicli.Command) *minicli.Response {
@@ -47,11 +120,7 @@ func cliQos(c *minicli.Command) *minicli.Response {
 
 	// Wildcard command
 	if tap == "all" {
-		if c.BoolArgs["remove"] {
-			qosRemoveAll()
-		} else {
-			resp.Error = fmt.Sprintf("not implemented")
-		}
+		resp.Error = fmt.Sprintf("not implemented")
 		return resp
 	}
 
@@ -68,59 +137,45 @@ func cliQos(c *minicli.Command) *minicli.Response {
 		return resp
 	}
 
-	if c.BoolArgs["add"] {
-		if t.qos == nil {
-			t.qos = newQos()
-		} else {
-			t.qos.change = true
+	if t.qos == nil {
+		t.qos = newQos()
+	} else {
+		t.qos.change = true
+	}
+
+	// Drop packets randomly with probability = loss
+	if c.BoolArgs["loss"] {
+		loss := c.StringArgs["percent"]
+
+		_, err := strconv.ParseFloat(loss, 64)
+		if err != nil {
+			resp.Error = fmt.Sprintf("`%s` is not a valid loss percentage", loss)
+			return resp
 		}
+		t.qos.params["loss"] = loss
+	}
 
-		// Drop packets randomly with probability = loss
-		if c.BoolArgs["loss"] {
-			loss := c.StringArgs["percent"]
+	// Add delay of time duration to each packet
+	if c.BoolArgs["delay"] {
+		delay := c.StringArgs["duration"]
+		_, err := time.ParseDuration(delay)
 
-			_, err := strconv.ParseFloat(loss, 64)
-			if err != nil {
-				resp.Error = fmt.Sprintf("`%s` is not a valid loss percentage", loss)
+		if err != nil {
+			if strings.Contains(err.Error(), "time: missing unit in duration") {
+				// Default to ms
+				delay = fmt.Sprintf("%s%s", delay, "ms")
+			} else {
+				resp.Error = fmt.Sprintf("`%s` is not a valid delay parameter", delay)
 				return resp
 			}
-			t.qos.params["loss"] = loss
 		}
+		t.qos.params["delay"] = delay
+	}
 
-		if c.BoolArgs["delay"] {
-			delay := c.StringArgs["duration"]
-			_, err:= time.ParseDuration(delay)
-
-			if err != nil {
-				if strings.Contains(err.Error(), "time: missing unit in duration") {
-					// Default to ms
-					delay = fmt.Sprintf("%s%s", delay, "ms")
-				} else {
-					resp.Error = fmt.Sprintf("`%s` is not a valid delay parameter", delay)
-					return resp
-				}
-			}
-			t.qos.params["delay"] = delay
-		}
-
-		// Execute the qos command
-		err := t.qosCmd()
-		if err != nil {
-			resp.Error = err.Error()
-		}
-
-	} else {
-		// Remove command
-		// Only remove qos from taps which had previous constraints
-		if t.qos == nil {
-			return resp
-		} else {
-			t.qos = nil
-		}
-		err := t.qosCmd()
-		if err != nil {
-			resp.Error = err.Error()
-		}
+	// Execute the qos command
+	err = t.qosCmd()
+	if err != nil {
+		resp.Error = err.Error()
 	}
 
 	return resp
