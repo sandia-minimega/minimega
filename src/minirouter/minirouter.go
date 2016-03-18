740b5b9b3c8df9623857e95684b09a3f1cf1b828
David Fritz <djfritz@sandia.gov>
2016-03-17 19:40:24 -0700

    minirouter skeleton

---
+++ src/minirouter/minirouter.go
@@ -0,0 +1,19 @@
+// minirouter is a simple wrapper daemon for bird and dnsmasq. It listens on a
+// unix domain socket for updates and in turn updates configs for downstream
+// services. minirouter also gathers statistics to populate vm tags on the host
+// minimega instance and for use by the 'router' API in minimega.
+package main
+
+func main () {
+	// flags
+
+	// update a running minirouter
+	if *f_e {
+
+		return
+	}
+
+	// start the domain socket service
+
+	// signal handling
+}
