cf29792f717b596d2490523028b74fd3768c88de
David Fritz <djfritz@sandia.gov>
2016-06-08 07:03:13 -0700

    starting minirouter for real

--- src/minirouter/minirouter.go
+++ src/minirouter/minirouter.go
@@ -1,17 +1,24 @@
-// minirouter is a simple wrapper daemon for bird and dnsmasq. It listens on a
+// minirouter is a simple wrapper daemon for bird, dnsmasq, and iptool. It listens on a
 // unix domain socket for updates and in turn updates configs for downstream
 // services. minirouter also gathers statistics to populate vm tags on the host
 // minimega instance and for use by the 'router' API in minimega.
 package main
 
+import (
+	"flag"
+)
+
+var (
+	f_loglevel   = flag.String("level", "warn", "set log level: [debug, info, warn, error, fatal]")
+	f_log        = flag.Bool("v", true, "log on stderr")
+	f_logfile    = flag.String("logfile", "", "also log to file")
+)
+
 func main () {
 	// flags
+	flag.Parse()
 
-	// update a running minirouter
-	if *f_e {
-
-		return
-	}
+	logSetup()
 
 	// start the domain socket service
 
