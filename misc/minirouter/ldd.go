89f9e3422deac09b9669532f19df191060ebf8c7
David Fritz <djfritz@sandia.gov>
2016-07-07 10:06:04 -0700

    misc: add minirouterfs build scripts

---
+++ misc/minirouter/ldd.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	p := os.Args[1]
+	d := os.Args[2]
+	o, err := exec.Command("ldd", p).Output()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	b := bytes.NewBuffer(o)
+
+	s := bufio.NewScanner(b)
+	for s.Scan() {
+		f := strings.Fields(s.Text())
+		var l string
+		if len(f) == 2 {
+			l = f[0]
+			if filepath.Dir(l) == "." {
+				continue
+			}
+		} else {
+			l = f[2]
+		}
+		dst := filepath.Join(d, filepath.Dir(l))
+		err := os.MkdirAll(dst, 0755)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		err = exec.Command("cp", l, dst).Run()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+	if err := s.Err(); err != nil {
+		fmt.Println(err)
+		return
+	}
+}
