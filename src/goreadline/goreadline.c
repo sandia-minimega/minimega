7b16ef8843d85dbfc24b8a7341b393f29d4739a8
Jon Crussell <joncrussell@gmail.com>
2016-08-01 07:49:10 -0700

    goreadline: fix copyright header
    Add missing lines.

--- src/goreadline/goreadline.c
+++ src/goreadline/goreadline.c
@@ -1,4 +1,6 @@
 // Copyright (2016) Sandia Corporation.
+// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// the U.S. Government retains certain rights in this software.
 
 #include <stdio.h>
 #include <unistd.h>
