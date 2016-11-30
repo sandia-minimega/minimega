f8ac875d06212a4dacb1f58451b11493f5b87c11
Jason Gao <jhgao@sandia.gov>
2016-11-29 17:45:43 -0800

    Add go vet to checks. Tweak build scripts. Fix versioning to pass gofmt. Make clean.bash safe to call from outside repo directory, after finding out the hard way.

--- test.bash
+++ test.bash
@@ -4,16 +4,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 . $SCRIPT_DIR/env.bash
 
-# set the version from the repo
-VERSION=`git rev-parse HEAD`
-DATE=`date --rfc-3339=date`
-echo "package version
-
-var (
-	Revision = \"$VERSION\"
-	Date = \"$DATE\"
-)
-" > $SCRIPT_DIR/src/version/version.go
+. $SCRIPT_DIR/addversion.bash
 
 # testing
 echo TESTING
---
+++ vet.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "CHECKING SOURCE CODE (go vet)"
+# note: we redirect go vet's output on STDERR to STDOUT
+OUTPUT="$(find . ! \( -path './src/vendor' -prune \) -type f -name '*.go' -exec go vet {} 2>&1 \;)"
+VET_EXIT=0
+if [ -n "$OUTPUT" ]; then
+    echo "$OUTPUT"
+    echo "go vet - FAILED"
+    echo
+    exit 1
+fi
+echo "go vet - OK"
+echo
