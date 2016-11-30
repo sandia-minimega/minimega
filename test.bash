771c5861b3dd07feb5a358f25b358c3717e391d5
Jason Gao <jhgao@sandia.gov>
2016-11-29 18:02:16 -0800

    Add go vet check to Travis CI. Add error exit codes to test.bash and doc.bash, so they properly break builds as appropriate on Travis CI.

--- test.bash
+++ test.bash
@@ -7,8 +7,25 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $SCRIPT_DIR/addversion.bash
 
 # testing
-echo TESTING
+echo "TESTING"
 for i in `ls $SCRIPT_DIR/src | grep -v vendor`
 do
 	go test $i
+	if [[ $? != 0 ]]; then
+	    PACKAGES_TEST_ERRORS=true
+	fi
 done
+
+# test python bindings
+$SCRIPT_DIR/misc/python/test_minimega.py &> /dev/null
+if [[ $? != 0 ]]; then
+    echo -e "FAIL\tminimega.py"
+    PYTHON_BINDINGS_TEST_ERRORS=true
+else
+    echo -e "ok\tminimega.py"
+fi
+echo
+
+if [ "$PACKAGES_TEST_ERRORS" = "true" ] || [ "$PYTHON_BINDINGS_TEST_ERRORS" = "true" ]; then
+    exit 1
+fi
