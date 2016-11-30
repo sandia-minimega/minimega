d8e3616ccd67472be572efbcc14cc64887716c01
Jason Gao <jhgao@sandia.gov>
2016-11-29 17:45:43 -0800

    Add go vet to checks. Tweak build scripts. Fix versioning to pass gofmt. Make clean.bash safe to call from outside repo directory, after finding out the hard way.

--- all.bash
+++ all.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 bash checkformat.bash
+bash vet.bash
 bash build.bash
 bash test.bash
 bash doc.bash
--- checkformat.bash
+++ checkformat.bash
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+echo "CHECKING SOURCE CODE (gofmt)"
 OUTPUT="$(find . ! \( -path './src/vendor' -prune \) -type f -name '*.go' -exec gofmt -d -l {} \;)"
 if [ -n "$OUTPUT" ]; then
     echo "$OUTPUT"
-    echo "gofmt - FAIL"
+    echo "gofmt - FAILED"
+    echo
     exit 1
 fi
+echo "gofmt - OK"
 echo
