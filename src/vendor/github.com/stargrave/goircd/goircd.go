84ebb9674a25e84000fd099d6b968e222bbfbeb8
Darek <x64x6a@users.noreply.github.com>
2016-12-01 16:50:52 -0800

    vendor: converted goircd from a program to a package
    For the most part, the package name was changed to goircd and the ability to edit the global setting values was added.

--- src/vendor/github.com/stargrave/goircd/goircd.go
+++ src/vendor/github.com/stargrave/goircd/goircd.go
@@ -16,121 +16,44 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-package main
-
-import (
-	"crypto/tls"
-	"flag"
-	"io/ioutil"
-	"log"
-	"net"
-	"path"
-	"path/filepath"
-	"strings"
-)
+package goircd
 
 var (
 	version   string
-	hostname  = flag.String("hostname", "localhost", "Hostname")
-	bind      = flag.String("bind", ":6667", "Address to bind to")
-	motd      = flag.String("motd", "", "Path to MOTD file")
-	logdir    = flag.String("logdir", "", "Absolute path to directory for logs")
-	statedir  = flag.String("statedir", "", "Absolute path to directory for states")
-	passwords = flag.String("passwords", "", "Optional path to passwords file")
-	tlsBind   = flag.String("tlsbind", "", "TLS address to bind to")
-	tlsPEM    = flag.String("tlspem", "", "Path to TLS certificat+key PEM file")
-	verbose   = flag.Bool("v", false, "Enable verbose logging.")
+	hostname  *string
+	bind      *string
+	motd      *string
+	logdir    *string
+	statedir  *string
+	passwords *string
+	tlsBind   *string
+	tlsPEM    *string
+	verbose   *bool
+
+	LogSink   = logSink
+	StateSink = stateSink
 )
 
-func listenerLoop(sock net.Listener, events chan ClientEvent) {
-	for {
-		conn, err := sock.Accept()
-		if err != nil {
-			log.Println("Error during accepting connection", err)
-			continue
-		}
-		client := NewClient(conn)
-		go client.Processor(events)
-	}
+type Settings struct {
+	Hostname  string
+	Bind      string
+	Motd      string
+	Logdir    string
+	Statedir  string
+	Passwords string
+	TlsBind   string
+	TlsPEM    string
+	Verbose   bool
 }
 
-func Run() {
-	events := make(chan ClientEvent)
-	log.SetFlags(log.Ldate | log.Lmicroseconds | log.Lshortfile)
-
-	if *logdir == "" {
-		// Dummy logger
-		go func() {
-			for _ = range logSink {
-			}
-		}()
-	} else {
-		if !path.IsAbs(*logdir) {
-			log.Fatalln("Need absolute path for logdir")
-		}
-		go Logger(*logdir, logSink)
-		log.Println(*logdir, "logger initialized")
-	}
-
-	log.Println("goircd " + version + " is starting")
-	if *statedir == "" {
-		// Dummy statekeeper
-		go func() {
-			for _ = range stateSink {
-			}
-		}()
-	} else {
-		if !path.IsAbs(*statedir) {
-			log.Fatalln("Need absolute path for statedir")
-		}
-		states, err := filepath.Glob(path.Join(*statedir, "#*"))
-		if err != nil {
-			log.Fatalln("Can not read statedir", err)
-		}
-		for _, state := range states {
-			buf, err := ioutil.ReadFile(state)
-			if err != nil {
-				log.Fatalf("Can not read state %s: %v", state, err)
-			}
-			room, _ := RoomRegister(path.Base(state))
-			contents := strings.Split(string(buf), "\n")
-			if len(contents) < 2 {
-				log.Printf("State corrupted for %s: %q", *room.name, contents)
-			} else {
-				room.topic = &contents[0]
-				room.key = &contents[1]
-				log.Println("Loaded state for room", *room.name)
-			}
-		}
-		go StateKeeper(*statedir, stateSink)
-		log.Println(*statedir, "statekeeper initialized")
-	}
-
-	if *bind != "" {
-		listener, err := net.Listen("tcp", *bind)
-		if err != nil {
-			log.Fatalf("Can not listen on %s: %v", *bind, err)
-		}
-		log.Println("Raw listening on", *bind)
-		go listenerLoop(listener, events)
-	}
-	if *tlsBind != "" {
-		cert, err := tls.LoadX509KeyPair(*tlsPEM, *tlsPEM)
-		if err != nil {
-			log.Fatalf("Could not load TLS keys from %s: %s", *tlsPEM, err)
-		}
-		config := tls.Config{Certificates: []tls.Certificate{cert}}
-		listenerTLS, err := tls.Listen("tcp", *tlsBind, &config)
-		if err != nil {
-			log.Fatalf("Can not listen on %s: %v", *tlsBind, err)
-		}
-		log.Println("TLS listening on", *tlsBind)
-		go listenerLoop(listenerTLS, events)
-	}
-	Processor(events, make(chan struct{}))
-}
-
-func main() {
-	flag.Parse()
-	Run()
+func SetSettings(settings Settings) {
+	hostname = &settings.Hostname
+	bind = &settings.Bind
+	motd = &settings.Motd
+	logdir = &settings.Logdir
+	statedir = &settings.Statedir
+	passwords = &settings.Passwords
+	tlsBind = &settings.TlsBind
+	tlsPEM = &settings.TlsPEM
+	verbose = &settings.Verbose
 }
