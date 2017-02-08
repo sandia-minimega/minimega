e38dfcf6a8a84683b6451c28cbb07241176a38c4
David Fritz <djfritz@sandia.gov>
2017-02-08 15:13:18 -0800

    doc: add miniplumber slidedeck

---
+++ doc/content/presentations/miniplumber_content/example1.mm
@@ -0,0 +1,15 @@
+# create a pipeline connecting foo and bar
+plumb foo "sed -u s/foo/moo/" bar
+
+plumb
+
+# write something to foo
+pipe foo "the cow says foo"
+
+pipe
+
+# works from the command line too
+# echo "hello plumber!" | minimega -pipe foo
+
+clear plumb
+clear pipe
