a0525a8efb31ba4f2ea8255374893f7956c0e06e
Jon Crussell <jcrusse@sandia.gov>
2017-02-13 10:28:00 -0800

    apigen/doc: generate API docs for minirouter
    Made apigen more flexible so that we can generate an API article for
    minirouter. minimega article looks the same as before but now there's a
    few flags that need to be specified.

--- doc.bash
+++ doc.bash
@@ -4,5 +4,11 @@ echo "BUILD DOCUMENTATION"
 
 # build api documentation
 echo "apigen"
-bin/apigen > doc/content/articles/api.article
+bin/apigen -bin bin/minimega \
+            -template doc/content_templates/minimega_api.template \
+            -sections .,mesh,vm,host \
+            > doc/content/articles/api.article
+bin/apigen -bin bin/minirouter \
+            -template doc/content_templates/minirouter_api.template \
+            > doc/content/articles/minirouter_api.article
 exit $?
