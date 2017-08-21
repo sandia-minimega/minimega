e24de107d72dc175c2671246f13ca0e16e325bba
Jon Crussell <jcrusse@sandia.gov>
2017-08-21 15:40:54 -0700

    miniweb: add flag to enable console
    Console is disabled by default. Use -console to enable.

--- src/miniweb/main.go
+++ src/miniweb/main.go
@@ -27,9 +27,10 @@ Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
 the U.S. Government retains certain rights in this software.`
 
 var (
-	f_addr = flag.String("addr", defaultAddr, "listen address")
-	f_root = flag.String("root", defaultRoot, "base path for web files")
-	f_base = flag.String("base", defaultBase, "base path for minimega")
+	f_addr    = flag.String("addr", defaultAddr, "listen address")
+	f_root    = flag.String("root", defaultRoot, "base path for web files")
+	f_base    = flag.String("base", defaultBase, "base path for minimega")
+	f_console = flag.Bool("console", false, "enable console")
 )
 
 var mm *miniclient.Conn
@@ -84,9 +85,16 @@ func main() {
 	mux.HandleFunc("/screenshot/", screenshotHandler)
 	mux.Handle("/ws/tunnel/", websocket.Handler(tunnelHandler))
 
-	mux.HandleFunc("/console", consoleHandler)
-	mux.HandleFunc("/console/", consoleHandler)
-	mux.Handle("/ws/console/", websocket.Handler(consoleWsHandler))
+	if *f_console {
+		mux.HandleFunc("/console", consoleHandler)
+		mux.HandleFunc("/console/", consoleHandler)
+		mux.Handle("/ws/console/", websocket.Handler(consoleWsHandler))
+	} else {
+		mux.HandleFunc("/console", func(w http.ResponseWriter, r *http.Request) {
+			http.Error(w, "console disabled, see -console flag", http.StatusNotImplemented)
+			return
+		})
+	}
 
 	server := &http.Server{
 		Addr:    *f_addr,
