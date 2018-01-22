d9085df1d2c8fe46a49f568eb633d81a58354829
cjsymon <csymonds@users.noreply.github.com>
2018-01-22 13:17:31 -0800

    works with limited testing. Some cleanup and documentation.

--- src/igor/stats.go
+++ src/igor/stats.go
@@ -17,13 +17,13 @@ import (
 
 var cmdStats = &Command{
 	UsageLine: "stats",
-	Short:     "calculate statistics given a date range",
+	Short:     "calculate statistics given a number of days prior to today",
 	Long: `
-Show usage statistics for the given date range. Limited to log history (~3 months)
+Show usage statistics for a range of days prior to today or duration of log history, whichever is shorter
 
 REQUIRED FLAGS:
 
-The -d flag specifies the number of days to be included in the report, ending with today. e.g. igor stats -d 7 will display statistics for the previous 7 days.
+The -d flag specifies the number of days to be included in the report, ending with today. e.g. igor stats -d 7 will display statistics for the previous 7 days (provided igor log goes back that far.)
 	`,
 }
 
@@ -32,49 +32,113 @@ var subD string // -d flag
 func init() {
 	// break init cycle
 	cmdStats.Run = runStats
-
 	cmdStats.Flag.StringVar(&subD, "d", "", "")
 
 }
 
-type Resdata struct {
-	User      string
-	ResStart  time.Time
-	ResEnd    time.Time
-	ActualEnd time.Time
-	Duration  int
-	Nodes     []string
+type ResData struct {
+	ResName        string
+	ResStart       time.Time
+	ResEnd         time.Time
+	ActualEnd      time.Time
+	ActualDuration time.Duration
+	Nodes          []string
 }
 
-type Stats struct {
-	// list of stats based on a user
+type Stats struct { //global statistics
 	NumRes               int
-	NodesUsed            map[string]bool
+	NodesUsed            map[string]int
 	NumNodes             int
-	TotalDurationMinutes int
+	NumUsers             int
+	TotalDurationMinutes time.Duration
+	TotalEarlyCancels    int
 }
 
 var (
-	reservations = map[string][]Resdata{}
+	formatDateStamp string = "2006/01/02"
+	formatTimeStamp string = "15:04:05"
+	formatLong      string = "2006-Jan-2-15:04"
+	statStartDate   time.Time
+	reservations    = map[string][]ResData{} //username->ResData
 )
 
-// Use nmap to scan all the nodes and then show which are up and the
-// reservations they below to
+// Helper function to capture reservation data from logs and store for processing
+func gatherInstallData(fields []string, s time.Time, e time.Time) ResData {
+	var err error
+	rd := ResData{}
+
+	rd.ResStart = s
+	rd.ResEnd = e
+	// ActualEnd and ActualDuration fields are updated after
+	// DELETED log entries are processed
+	// NOTE: If we never encounter a DELETED entry for a corresponding INSTALL entry,
+	// we calculate the reservation time during stats calculation (see RunStats)
+	rd.ResName = strings.TrimLeft(fields[6], "resname=")
+
+	// get nodes used in reservation TODO: Plot histogram of nodes used?
+	nodes := strings.TrimLeft(fields[7], "nodes=")
+	nodelist, err := ranges.SplitList(nodes)
+	if err != nil {
+		log.Fatal("%v", err)
+	}
+	rd.Nodes = nodelist
+	user := strings.TrimLeft(fields[5], "user=")
+	reservations[user] = append(reservations[user], rd)
+	return rd
+}
+
+func gatherDeleteData(fields []string, s time.Time, e time.Time) {
+	// The timestamp for the log indicates when this delete happened
+	ad, err := time.Parse(formatDateStamp, fields[0])
+	if err != nil {
+		log.Fatal("%v", err)
+	}
+	at, err := time.Parse(formatTimeStamp, fields[1])
+	if err != nil {
+		log.Fatal("%v", err)
+	}
+	ae := time.Date(ad.Year(), ad.Month(), ad.Day(), at.Hour(), at.Minute(), at.Second(), 0, at.Location())
+	fmt.Printf("ae: %v\nstatStartDate: %v\n", ae, statStartDate)
+	// if it was deleted before our stat range, we don't care about this
+	if ae.Before(statStartDate) {
+		return
+	}
+
+	resName := strings.TrimLeft(fields[6], "resname=")
+	user := strings.TrimLeft(fields[5], "user=")
+	notFound := true
+	for i, r := range reservations[user] {
+		if r.ResStart == s && r.ResName == resName {
+			// this is a delete for a res we know about
+			r.ActualEnd = ae
+			reservations[user][i] = r
+			notFound = false
+		}
+	}
+	if notFound {
+		// We did not know about this reservation
+		// the log was likely reset after it started
+		rd := gatherInstallData(fields, s, e)
+		rd.ActualEnd = ae
+		rd.ActualDuration = ae.Sub(statStartDate)
+		for i, r := range reservations[user] {
+			if r.ResStart == s && r.ResName == resName {
+				reservations[user][i] = rd
+			}
+		}
+	}
+}
+
+// Parses igor log for reservation data and calculates usage statistic
+// for the time duration specified. Output stats to standard out
 func runStats(_ *Command, _ []string) {
 
 	// Parse flags
-	duration := 0
 	d, err := strconv.Atoi(subD)
 	if err != nil {
 		log.Fatalln("Invalid duration specified")
 	}
-	duration = d
-
-	formatshort := "2006/01/02"
-	formatlong := "2006-Jan-2-15:04"
-	now := time.Now()
-	start := now.AddDate(0, 0, -duration)
-	//fmt.Printf("Start: %v; Now: %v\n", start, now)
+	statStartDate = time.Now().AddDate(0, 0, -d)
 
 	// open and read in log file
 	f, err := os.Open(igorConfig.LogFile)
@@ -82,76 +146,90 @@ func runStats(_ *Command, _ []string) {
 		log.Fatal("Unable to read in log file: %v", err)
 	}
 	defer f.Close()
-	// parse log
+	// parse log and build data structs
 	s := bufio.NewScanner(f)
-	statMap := map[string]Stats{} //user->Stats
 	for s.Scan() {
 		line := s.Text()
 		fields := strings.Fields(line)
-		if len(fields) < 2 {
+		if len(fields) < 9 {
 			continue
 		}
-		tStamp, err := time.Parse(formatshort, fields[0])
+
+		resStart, err := time.Parse(formatLong, strings.TrimLeft(fields[8], "start="))
 		if err != nil {
-			//fmt.Printf("Error parsing time stamp: %v\n", err)
-			continue
+			continue //not a log entry we care about
 		}
-		if tStamp.Before(start) {
-			continue
+
+		resEnd, err := time.Parse(formatLong, strings.TrimLeft(fields[9], "end="))
+		if err != nil {
+			log.Fatal("%v", err) //if this failed, something is wrong
+		}
+
+		if resEnd.Before(statStartDate) {
+			continue // This ended before the range we care about
 		}
 
 		for _, a := range fields {
 			if a == "INSTALL" {
-				var st Stats
-				var err error
-				data := Resdata{}
-				data.User = strings.TrimLeft(fields[5], "user=")
-				resname := strings.TrimLeft(fields[6], "resname=")
-				nodes := strings.TrimLeft(fields[7], "nodes=")
-				data.ResStart, err = time.Parse(formatlong, strings.TrimLeft(fields[8], "start="))
-				if err != nil {
-					log.Fatal("%v", err)
-				}
-				data.ResEnd, err = time.Parse(formatlong, strings.TrimLeft(fields[9], "end="))
-				if err != nil {
-					log.Fatal("%v", err)
-				}
-				data.Duration, err = strconv.Atoi(strings.TrimLeft(fields[10], "duration="))
-				if err != nil {
-					log.Fatal("Error converting duration: %v", err)
-				}
-				if s, ok := statMap[data.User]; ok {
-					st = s
-				} else {
-					st = Stats{}
-					st.NodesUsed = make(map[string]bool)
-				}
-				st.NumRes++
-				st.TotalDurationMinutes += duration
-				nodelist, err := ranges.SplitList(nodes)
-				if err != nil {
-					log.Fatal("%v", err)
-				}
-				for _, n := range nodelist {
-					if st.NodesUsed[n] != true {
-						st.NodesUsed[n] = true
-						st.NumNodes += 1
-					}
-				}
-				data.Nodes = nodelist
-				statMap[data.User] = st
-				reservations[resname] = append(reservations[resname], data)
+				gatherInstallData(fields, resStart, resEnd)
+			}
+			if a == "DELETED" {
+				gatherDeleteData(fields, resStart, resEnd)
 			}
 		}
-		//fmt.Printf("%v\n", tStamp)
 	}
 
-	// Build database
-	for k,v := range reservations {
-		fmt.Printf("\n%v\n", k)
-		for _, d := range v {
-			fmt.Printf("%v\n",d)
+	// Calculate Stats
+	globalStats := Stats{}
+	globalStats.NodesUsed = make(map[string]int)
+	for n, rd := range reservations {
+		var uResCount, uEarlyCancel int
+		var uDuration time.Duration
+
+		globalStats.NumUsers += 1
+		fmt.Printf("%v\n", n)
+		for _, d := range rd {
+			fmt.Printf("Res: %v\n", d.ResName)
+			for _, n := range d.Nodes {
+				fmt.Printf("%v\n", n)
+				globalStats.NodesUsed[n] += 1
+			}
+			fmt.Printf("ResStart: %v, ResEnd: %v\n", d.ResStart, d.ResEnd)
+			if d.ActualDuration.Minutes() == 0 { // we never saw this res get deleted
+				if statStartDate.Before(d.ResStart) {
+					uDuration += time.Now().Sub(d.ResStart)
+				} else {
+					uDuration += time.Now().Sub(statStartDate)
+				}
+			} else {
+				uDuration += d.ActualDuration
+			}
+			uResCount += 1
+			earlyCancel := false
+			// fuzzy math here because igor log uses 2 different granularities of timestamps
+			// so direct comparisons won't work
+			//TODO: Fix timestamp inconsistencies in igor logs
+			if (d.ResEnd.Sub(d.ActualEnd).Minutes()) < 1.0 {
+				earlyCancel = true
+				uEarlyCancel += 1
+			}
+			fmt.Printf("Actual End: %v, Actual Duration: %v\n", d.ActualEnd, uDuration)
+			fmt.Printf("Early Cancel: %v\n\n", earlyCancel)
+		}
+		globalStats.NumRes += uResCount
+		globalStats.TotalDurationMinutes += uDuration
+		globalStats.TotalEarlyCancels += uEarlyCancel
+	}
+	for _, d := range globalStats.NodesUsed {
+		if d > 0 {
+			globalStats.NumNodes += 1
 		}
 	}
-	fmt.Printf("\n%v\n", statMap)
+
+	//Repoort
+	fmt.Printf("Total Users: %v\n", globalStats.NumUsers)
+	fmt.Printf("Total Number of Reservations: %v\n", globalStats.NumRes)
+	fmt.Printf("Total Number of Nodes Used: %v\n", globalStats.NumNodes)
+	fmt.Printf("Total Number of Reservations Canceled Early: %v\n", globalStats.TotalEarlyCancels)
+	fmt.Printf("Total Reservations Time: %v\n", globalStats.TotalDurationMinutes)
 }
