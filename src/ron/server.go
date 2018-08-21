157d8a0ec9c55430f1b97da056dc792ac855e819
Jon Crussell <jcrusse@sandia.gov>
2018-08-21 13:38:01 -0700

    ron: fix panic, again
    Need to check that the filter is also not nil...

--- src/ron/server.go
+++ src/ron/server.go
@@ -918,7 +918,7 @@ func (s *Server) route(m *Message) {
 
 				// load the relevant info fields, overriding any tag values
 				for _, cmd := range m.Commands {
-					if cmd.Filter.Tags != nil {
+					if cmd.Filter != nil && cmd.Filter.Tags != nil {
 						for k := range cmd.Filter.Tags {
 							// only replace non-zero fields
 							v, _ := vm.Info(k)
