879ec38d77e3c79a443574e0a7f43cbdbd927870
Jon Crussell <jcrusse@sandia.gov>
2019-03-12 06:13:30 -0700

    vnc: remove empty file
    Functionality moved.

--- src/vnc/client.go
+++
@@ -1,7 +0,0 @@
-// Copyright (2015) Sandia Corporation.
-// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
-// the U.S. Government retains certain rights in this software.
-
-package vnc
-
-import ()
