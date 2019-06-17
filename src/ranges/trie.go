9fa974bd9b28df8a329bb11b06897fd0dc017928
Jon Crussell <jcrusse@sandia.gov>
2019-06-17 12:11:38 -0700

    ranges: fun with recursion
    Find repeated patterns in suffixes and collapse them.

--- src/ranges/trie.go
+++ src/ranges/trie.go
@@ -6,6 +6,7 @@ package ranges
 
 import (
 	"fmt"
+	"sort"
 	"unicode"
 )
 
@@ -75,6 +76,105 @@ func (t *trieNode) AlphaPrefixes() []string {
 
 	if t.Terminal || numericChild {
 		res = append(res, "")
+	}
+
+	return res
+}
+
+// Flatten returns a compact representation of the trie with numbers replaced
+// with range notations
+func (t *trieNode) Flatten() []string {
+	var res []string
+
+	if t.Terminal {
+		res = append(res, "")
+	}
+
+	children := t.sortedChildren()
+
+	// children that are numbers and their descendants that are non-numbers
+	nums := map[int]*trieNode{}
+
+	for _, k := range children {
+		// record numeric children for later
+		if unicode.IsNumber(k) {
+			for i, child := range t.Children[k].flattenNums(k) {
+				nums[i] = child
+			}
+
+			continue
+		}
+
+		// append non-numeric children immediately
+		for _, suffix := range t.Children[k].Flatten() {
+			res = append(res, fmt.Sprintf("%c%s", k, suffix))
+		}
+	}
+
+	// suffixes and the ints that they correspond to
+	vals := map[string][]int{}
+
+	for i, descendant := range nums {
+		for _, suffix := range descendant.Flatten() {
+			vals[suffix] = append(vals[suffix], i)
+		}
+	}
+
+	// again, sort for determinism
+	suffixes := []string{}
+
+	for suffix := range vals {
+		suffixes = append(suffixes, suffix)
+	}
+
+	sort.Strings(suffixes)
+
+	// combine each group with the same suffix
+	for _, suffix := range suffixes {
+		res = append(res, unsplitInts(vals[suffix])+suffix)
+	}
+
+	return res
+}
+
+// sortedChildren returns the children rune in sorted order
+func (t *trieNode) sortedChildren() []rune {
+	// runes of children to sort
+	var res []rune
+
+	// flatten all the children that are numbers first
+	for k := range t.Children {
+		res = append(res, k)
+	}
+
+	// sort child runes so that there's some determinism on ordering
+	sort.Slice(res, func(i, j int) bool {
+		return res[i] < res[j]
+	})
+
+	return res
+}
+
+// flattenNums converts trieNodes from a given point into ints and returns
+// pointers to the children. If r is not a number, returns an empty map.
+func (t *trieNode) flattenNums(r rune) map[int]*trieNode {
+	res := map[int]*trieNode{}
+
+	// value of this rune
+	i := int(r - '0')
+
+	for k, child := range t.Children {
+		if !unicode.IsNumber(k) {
+			continue
+		}
+
+		for k, v := range child.flattenNums(k) {
+			res[i*10+k] = v
+		}
+	}
+
+	if len(res) == 0 {
+		res[i] = t
 	}
 
 	return res
