59f2aeaa3d2954da892771d72a35b888f1deaf80
Michael Wisely <mwisely@users.noreply.github.com>
2019-08-12 17:32:04 -0500

    Change column sizes

--- src/igorweb/static/scripts/ReservationInfo.js
+++ src/igorweb/static/scripts/ReservationInfo.js
@@ -3,17 +3,17 @@
     <!-- Container of node grid, key, and reservation table -->
     <div class="container-fluid" style="margin-top:10px;">
       <div class="row">
-        <div class="col-12 col-lg-6">
+        <div class="col-12 col-xl-6">
           <node-grid></node-grid>
         </div>
-        <div class="col-12 col-lg-6">
+        <div class="col-12 col-xl-6">
           <div class="row">
-            <div class="col-3 col-lg-3"></div>
+            <div class="col-3"></div>
             <!-- offset -->
-            <div class="col-6 col-lg-6">
+            <div class="col-6">
               <key-card></key-card>
             </div>
-            <div class="col-12 col-lg-12">
+            <div class="col-12">
               <div class="card mx-auto">
                 <div class="card-body">
                   <div class="input-group">
