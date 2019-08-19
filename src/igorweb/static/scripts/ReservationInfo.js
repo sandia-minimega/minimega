86fa122fd964e6c0d01468a3d6ed20b958bc27ca
Michael Wisely <mwisely@users.noreply.github.com>
2019-08-19 08:54:01 -0500

    Change margins for ReservationInfo component

--- src/igorweb/static/scripts/ReservationInfo.js
+++ src/igorweb/static/scripts/ReservationInfo.js
@@ -1,7 +1,7 @@
 (function() {
   const template = `
     <!-- Container of node grid, key, and reservation table -->
-    <div class="container-fluid" style="margin-top:10px;">
+    <div class="container-fluid" style="margin-top:30px;">
       <div class="row">
         <div class="col-12 col-xl-6">
           <node-grid></node-grid>
