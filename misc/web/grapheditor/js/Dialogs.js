371735902874299b937b6b08085f81d3684e8f76
Arthur Hernandez <aherna4@.sandia.gov>
2020-02-19 11:34:54 -0800

    update 0.3

--- misc/web/grapheditor/js/Dialogs.js
+++ misc/web/grapheditor/js/Dialogs.js
@@ -2946,6 +2946,7 @@ var EditMiniConfigDialog = function(editorUi,vertices,edges)
 		count++;
 
 		var clear ="";
+
 		// Generate configuration for parameters
 		for (const p in parameters) {
 			// If there is no configuration for a parameter and the previous device had one clear it
@@ -2957,11 +2958,26 @@ var EditMiniConfigDialog = function(editorUi,vertices,edges)
 			}
 			// if it has a configuration for the parameter set it else issue a default value
 			if (cell.getAttribute(p) != undefined && cell.getAttribute(p) != "undefined") { 
-				//tmp = `vm config ${p} ${cell.getAttribute(p)} \n`;
 				if(prev_dev[p] != cell.getAttribute(p)){
-					//dev_config+=tmp;
 					prev_dev[p] = cell.getAttribute(p);
 					config += `vm config ${p} ${cell.getAttribute(p)} \n`;
+					continue;
+				}
+				var net ="";
+				for (var i =0; i< cell.getEdgeCount();i++){
+					var e = cell.getEdgeAt(i);
+					net += `${e.getAttribute("vlan")} `;
+				}
+				if (net == ""){
+					delete prev_dev[p];
+					clear += "clear vm config network"
+				}
+				else {
+					cell.setAttribute("network",net)
+					if (!prev_dev["network"] != net){
+					prev_dev["network"] = net
+					config += `vm config network ${net} \n`;
+					}
 				}
 			}
 			else {
@@ -2975,29 +2991,6 @@ var EditMiniConfigDialog = function(editorUi,vertices,edges)
 		  }
 		
 		config += clear;
-		var net ="";
-		for (var i =0; i< cell.getEdgeCount();i++){
-			var e = cell.getEdgeAt(i);
-			net += `${e.getAttribute("vlan")} `;
-		}
-		if (net == ""){
-			clear += "clear vm config network"
-		}
-		else {
-			cell.setAttribute("network",net)
-
-		//var tmp = `vm config network ${net} \n`;
-			if (!prev_dev["network"] != net){
-			//dev_config+=tmp;
-				prev_dev["network"] = net
-				config += `vm config network ${net} \n`;
-			}
-		}
-
-		/*if (dev_config != ""){
-			prev_dev_config += dev_config;
-		}*/
-
 		if (cell.getStyle().includes("container")){config+=`vm launch container ${name}\n\n`;}
 		else {config+=`vm launch kvm ${name}\n\n`;}
 	});
