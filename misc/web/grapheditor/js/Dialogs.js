9a3d56ff1b2c3d39e79d7119173687e863b90363
Arthur Hernandez <aherna4@.sandia.gov>
2020-02-19 11:17:52 -0800

    update 0.1

--- misc/web/grapheditor/js/Dialogs.js
+++ misc/web/grapheditor/js/Dialogs.js
@@ -2923,7 +2923,7 @@ var EditMiniConfigDialog = function(editorUi,vertices,edges)
 	var parameters = {memory:"2048", vcpu:"1", network:undefined,kernel:undefined,initrd:undefined,disk:undefined,snapshot:true,cdrom:undefined};
 	var config = "";
 	var prev_dev_config = "";
-	var prev_dev;
+	var prev_dev = {};
 	vertices.forEach(cell => {
 		var dev_config="";
 		var name = "";
@@ -2949,20 +2949,25 @@ var EditMiniConfigDialog = function(editorUi,vertices,edges)
 		// Generate configuration for parameters
 		for (const p in parameters) {
 			// If there is no configuration for a parameter and the previous device had one clear it
-			if ((cell.getAttribute(p) == undefined || cell.getAttribute(p) == "undefined") && prev_dev_config.includes(`vm config ${p}`)){
-				clear +=`clear vm config ${p}\n`;
+			if ((cell.getAttribute(p) == undefined || cell.getAttribute(p) == "undefined") && prev_dev.hasOwnProperty(p)){
+				if (p != "network"){
+					clear +=`clear vm config ${p}\n`;
+				}
 			}
 			// if it has a configuration for the parameter set it else issue a default value
 			if (cell.getAttribute(p) != undefined && cell.getAttribute(p) != "undefined") { 
-				tmp = `vm config ${p} ${cell.getAttribute(p)} \n`;
-				if(!prev_dev_config.includes(tmp)){
-					dev_config+=tmp;
-					config += tmp;
+				//tmp = `vm config ${p} ${cell.getAttribute(p)} \n`;
+				if(prev_dev[p] != cell.getAttribute(p)){
+					//dev_config+=tmp;
+					prev_dev[p] = cell.getAttribute(p);
+					config += `vm config ${p} ${cell.getAttribute(p)} \n`;
 				}
 			}
 			else {
-				if (parameters[p] != undefined && !prev_dev_config.includes(`vm config ${p} ${parameters[p]} \n`)){
-					dev_config += `vm config ${p} ${parameters[p]} \n`;
+				//if (parameters[p] != undefined && !prev_dev_config.includes(`vm config ${p} ${parameters[p]} \n`)){
+				if (parameters[p] != undefined && prev_dev[p] != parameters[p]){
+					//dev_config += `vm config ${p} ${parameters[p]} \n`;
+					prev_dev[p] = cell.getAttribute(p);
 					config += `vm config ${p} ${parameters[p]} \n`;
 				}
 			}
@@ -2974,16 +2979,23 @@ var EditMiniConfigDialog = function(editorUi,vertices,edges)
 			var e = cell.getEdgeAt(i);
 			net += `${e.getAttribute("vlan")} `;
 		}
+		if (net == ""){
+			clear += "clear vm config network"
+		}
+		else {
+			cell.setAttribute("network",net)
 
-		var tmp = `vm config network ${net} \n`;
-		if (!prev_dev_config.includes(tmp)){
-			dev_config+=tmp;
-			config += tmp;
+		//var tmp = `vm config network ${net} \n`;
+			if (!prev_dev["network"] != net){
+			//dev_config+=tmp;
+				prev_dev["network"] = net
+				config += `vm config network ${net} \n`;
+			}
 		}
 
-		if (dev_config != ""){
+		/*if (dev_config != ""){
 			prev_dev_config += dev_config;
-		}
+		}*/
 
 		if (cell.getStyle().includes("container")){config+=`vm launch container ${name}\n\n`;}
 		else {config+=`vm launch kvm ${name}\n\n`;}
