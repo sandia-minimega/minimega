8d6a5cde8cc04a00c732be906a1dd2afa65dfa6e
Christopher Symonds <symondschristopher@gmail.com>
2020-03-23 17:38:04 -0700

    final changes required to deploy under go 112

--- misc/appengine/update.bash
+++ misc/appengine/update.bash
@@ -3,7 +3,11 @@
 # stop on error
 
 URL=https://github.com/sandia-minimega/minimega.git
-ROOT=/home/cjs/go
+ROOT=$(go env GOPATH)
+
+if [ ! -d $ROOT/src ]; then
+    mkdir -p $ROOT/src
+fi
 
 REPO=$ROOT/src/minimega
 TARGET=$ROOT/src/mmorg
@@ -11,34 +15,44 @@ SDK=$ROOT/src/google-cloud-sdk
 
 PATH=$PATH:/usr/local/go/bin
 
-#if [ -d $REPO ]; then
-#    rm -rf $REPO
-#fi
-#
-#git clone $URL $REPO
-#(cd $REPO && bash all.bash)
-#
-#if [ -d $TARGET ]; then
-#    rm -rf $TARGET
-#fi
+if [ -d $REPO ]; then
+    rm -rf $REPO
+fi
 
-#mkdir $TARGET
+git clone $URL $REPO
+(cd $REPO && bash all.bash)
+
+if [ -d $TARGET ]; then
+    rm -rf $TARGET
+fi
+
+mkdir $TARGET
 
 # copy appengine
-#cp $REPO/misc/appengine/app.yaml $TARGET
-#cp $REPO/src/minidoc/* $TARGET
-#cp -r $REPO/doc $TARGET
+cp $REPO/misc/appengine/app.yaml $TARGET
+cp $REPO/misc/appengine/go.mod $TARGET
+cp $REPO/src/minidoc/* $TARGET
+cp -r $REPO/doc $TARGET
 
-# copy dependencies (used to be in $TARGET/src/)
-#cp -r $REPO/src/minicli $TARGET
-#cp -r $REPO/src/minilog $TARGET
-#cp -r $REPO/src/present $TARGET
-#cp -r $REPO/src/ranges $TARGET
-#mkdir -p $TARGET/golang.org/x/net
-#cp -r $REPO/src/vendor/golang.org/x/net/websocket $TARGET/golang.org/x/net/
+# copy dependencies
+cp -r $REPO/src/minicli $TARGET
+cp -r $REPO/src/minilog $TARGET
+cp -r $REPO/src/present $TARGET
+cp -r $REPO/src/ranges $TARGET
+mkdir -p $TARGET/golang.org/x/net
+cp -r $REPO/src/vendor/golang.org/x/net/websocket $TARGET/golang.org/x/net/
+
+# Update include paths
+# We need to do this because of the migration to go 112+ requires a specific directory structure
+# This structure is not supported by our structure in minimega, so this is a hack to make it work
+grep -rl \"minicli\" --exclude-dir minicli --exclude-dir doc $TARGET | xargs sed -i 's/\"minicli\"/\"mmorg\/minicli\"/'
+grep -rl \"minilog\" --exclude-dir minilog $TARGET | xargs sed -i 's/\"minilog\"/\"mmorg\/minilog\"/'
+grep -rl \"present\" --exclude-dir present $TARGET | xargs sed -i -e '0,/\"present\"/ s/\"present\"/\"mmorg\/present\"/'
+grep -rl \"ranges\" --exclude-dir ranges $TARGET | xargs sed -i 's/\"ranges\"/\"mmorg\/ranges\"/'
+sed -i 's/\"golang\.org/\"mmorg\/golang\.org/' $TARGET/socket.go
+
 
 # update tip.minimega.org
-#cd $TARGET/app
 cd $TARGET
 GOPATH=$ROOT $SDK/bin/gcloud app deploy --verbosity=debug --project pivotal-sonar-90317 --version 1
 
