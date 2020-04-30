9bfe39e5bd7799081313feedc0af1c35986a7f2b
keith <13208865+dnkcom@users.noreply.github.com>
2020-04-29 18:20:45 -0600

    Added struct function for FindNodesWithLabels.

--- phenix/types/version/v1/topology.go
+++ phenix/types/version/v1/topology.go
@@ -10,3 +10,21 @@ func (this *TopologySpec) SetDefaults() {
 		n.SetDefaults()
 	}
 }
+
+// FindNodesWithLabels finds all nodes in the topology containing at least one
+// of the labels provided. Take note that the node does not have to have all the
+// labels provided, just one.
+func (this TopologySpec) FindNodesWithLabels(labels ...string) []*Node {
+	var nodes []*Node
+
+	for _, n := range this.Nodes {
+		for _, l := range labels {
+			if _, ok := n.Labels[l]; ok {
+				nodes = append(nodes, n)
+				break
+			}
+		}
+	}
+
+	return nodes
+}
