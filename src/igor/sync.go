063bf1a810ac80584a592cb0d22944a438c1b7dd
msymond <msymond@sandia.gov>
2020-06-25 16:08:03 -0700

    sync is now non-destructive by default, removed -d flag, updated help

--- src/igor/sync.go
+++ src/igor/sync.go
@@ -16,27 +16,23 @@ var cmdSync = &Command{
 	UsageLine: "sync",
 	Short:     "synchronize igor data",
 	Long: `
-Does an internal check to verify the integrity of the data file. Can report and attempt to clean.
+Does an internal check to verify the integrity of the data file and produces a report. Can also attempt to force a sync.
 SYNOPSIS
-	igor sync <[-d] [-f]> [-q] WHAT
+	igor sync [-f] [-q] WHAT
 OPTIONS
 	-f, -force
-	    Will force sync to fix inconsistencies found in addition to reporting
-	-d, -dry-run
-	    Does not attempt to make any corrections, only reports inconsistencies
+	    If an inconsistentcy is found between the reservation and WHAT, will command WHAT to set its relevant attribute to match the reservation's value in addition to reporting
 	-q, -quiet
-	    Suppress reports, only report errors
+	    Suppress report of all data, only reports inconsistencies
 Possible WHATs:
 arista: 	reconfigure switchports for active reservations
 	`,
 }
 
-var subF bool   // -f
-var force bool  // -force
-var subD bool   // -d
-var dryRun bool // -dry-run
-var subQ bool   // -q
-var quiet bool  // -quiet
+var subF bool  // -f
+var force bool // -force
+var subQ bool  // -q
+var quiet bool // -quiet
 
 func init() {
 	// break init cycle
@@ -44,8 +40,6 @@ func init() {
 
 	cmdSync.Flag.BoolVar(&subF, "f", false, "")
 	cmdSync.Flag.BoolVar(&force, "force", false, "")
-	cmdSync.Flag.BoolVar(&subD, "d", false, "")
-	cmdSync.Flag.BoolVar(&dryRun, "dry-run", false, "")
 	cmdSync.Flag.BoolVar(&subQ, "q", false, "")
 	cmdSync.Flag.BoolVar(&quiet, "quiet", false, "")
 }
@@ -53,13 +47,8 @@ func init() {
 // Gather data integrity information, report, and fix
 func runSync(cmd *Command, args []string) {
 	// process flags
-	dryRun = (dryRun || subD)
 	force = (force || subF)
 	quiet = (quiet || subQ)
-
-	if dryRun == force {
-		log.Fatal("Missing or invalid flags. Please see igor sync -h, --help")
-	}
 
 	if igor.Username != "root" {
 		log.Fatalln("Sync access restricted. Please use as admin.")
@@ -87,50 +76,45 @@ func syncArista() {
 	// create writer
 	w := new(tabwriter.Writer)
 	w.Init(os.Stdout, 0, 0, 1, ' ', 0)
-	// print header if not quiet
-	if !quiet {
-		// need to print column labels as variables for format/alignment consistency
-		n := "NODE"
-		i := "IGOR"
-		a := "ARISTA"
-		fmt.Println("")
-		fmt.Fprintln(w, n, "\t", i, "  ", a)
-	}
+
+	// HEADER
+	// need to print column labels as variables for format/alignment consistency
+	n := "NODE"
+	i := "IGOR"
+	a := "ARISTA"
+	fmt.Println("")
+	fmt.Fprintln(w, n, "\t", i, "  ", a)
+
 	// TODO: probably shouldn't iterate over .M directly
 	for _, r := range igor.Reservations.M {
 		if !r.IsActive(igor.Now) {
 			continue
 		}
-
-		if !quiet {
-			// print all nodes, Igor VLANs, and arista VLANs
-			// associated with this reservation
-			for _, host := range r.Hosts {
-				vlan := strconv.Itoa(r.Vlan)
-				gtvlan := gt[host]
-				if gtvlan == "" {
-					gtvlan = "(none)"
-				}
-				if gtvlan != vlan {
-					vlan = FgRed + vlan + Reset
-				}
-				fmt.Fprintln(w, host, "\t", vlan, "  ", gtvlan)
-
+		// each reservation can have multiple hosts, but only 1 vlan
+		mismatch := false
+		for _, host := range r.Hosts {
+			vlan := strconv.Itoa(r.Vlan)
+			gtvlan := gt[host]
+			// if reservation does not match arista, color the value red to print
+			if gtvlan != vlan {
+				vlan = FgRed + vlan + Reset
+				mismatch = true
 			}
-		} else {
-			// just print what's different
-			for _, host := range r.Hosts {
-				vlan := strconv.Itoa(r.Vlan)
-				gtvlan := gt[host]
-				if gtvlan == "" {
-					gtvlan = "(none)"
-				}
-				if gtvlan != vlan {
-					fmt.Fprintln(w, "DISCREPANCY IN NODE: ", host, "\tIGOR VLAN: ", vlan, "\tARISTA VLAN: ", gtvlan)
+			// if arista had no vlan assigned, make explicit for readability
+			if gtvlan == "" {
+				gtvlan = "(none)"
+			}
+			if !quiet {
+				fmt.Fprintln(w, host, "\t", vlan, "  ", gtvlan)
+			} else {
+				if mismatch {
+					fmt.Fprintln(w, host, "\t", vlan, "  ", gtvlan)
 				}
 			}
 		}
-		if !dryRun {
+		// if force flag given and a discrepancy is detected between reservation and
+		// arista, then set node in arista with vlan value from the reservation
+		if force && mismatch {
 			if err := networkSet(r.Hosts, r.Vlan); err != nil {
 				log.Fatal("unable to set up network isolation")
 			}
