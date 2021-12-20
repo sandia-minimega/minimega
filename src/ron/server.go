sandia-minimega/minimega

commit dd04c33fc6417b6e8ee57c451c7efdd032f71283

miniccc: Improve reconnect logic for serial clients (#1459)
* miniccc: Improve reconnect logic for serial clients

The miniccc client and ron server have been updated to better support
reconnection capabilities when using the virtual serial port in QEMU
virtual machines. Past merge 850c4450 added support for reconnecting
over serial after a virtual machine restart, but didn't address
connection issues that arise after a VM has been paused or restored from
snapshot.

When a VM is paused, the server side of the serial connection eventually
disconnects and resets. When the VM is resumed, the client is still
connected to the virtual serial port in the VM but messages are no
longer making it to the server because of the server-side reset. Since
the virtual serial port in the client never changed (magic of QEMU
serial ports that are beyond my understanding), the client never sees an
EOF and is still able to write to the port without error.

The same thing as above happens when a VM is restored from snapshot...
the server side makes a new connection to the unix socket that's mapped
to the VM's virtual serial port, and the client is still connected to
the virtual serial port in the VM like it was prior to the snapshot.

In order to allow for the client to detect the disconnect, a HEARTBEAT
message type was added and the server was updated to send a HEARTBEAT
message to the client every so often (default is 5s). The client does
nothing with this message, but can expect to receive it consistently,
and can now timeout and reset if no messages are received within a
certain amount of time (default is 13s).

The Linux miniccc client is able to reset by simply closing its
connection to the virtual serial port and reconnecting. This approach
fails on Windows, however, and the only way to reconnect to the virtual
serial port on Windows is to restart the miniccc client process. The

src/ron/server.go
@@ -203,16 +203,16 @@ func (s *Server) ListenUnix(path string) error {
 
 // Dial a client serial port. The server will maintain this connection until a
 // client connects and then disconnects.
-func (s *Server) DialSerial(path string, ccPortOpened, ccPortClosed chan struct{}) error {
+func (s *Server) DialSerial(path, uuid string) error {
 	dial := func(path string) (net.Conn, error) {
 		log.Info("dial serial: %v", path)
 
 		s.connsLock.Lock()
 		defer s.connsLock.Unlock()
 
-		// are we already connected to this client?
-		if _, ok := s.conns[path]; ok {
-			return nil, fmt.Errorf("already connected to serial client %v", path)
+		// close any lingering connections to this client
+		if conn, ok := s.conns[path]; ok {
+			conn.Close()
 		}
 
 		// connect!
@@ -226,52 +226,43 @@ func (s *Server) DialSerial(path string, ccPortOpened, ccPortClosed chan struct{
 		return conn, nil
 	}
 
-	var (
-		conn net.Conn
-		cli  *client
-	)
-
+	// Monitor this client connection, reconnecting when the client handler
+	// completes if the VM still exists.
 	go func() {
 		for {
-			<-ccPortClosed
+			if _, ok := s.vms[uuid]; !ok {
+				log.Debug("vm %s (serial://%s) no longer exists -- closing serial connection", uuid, path)
 
-			if cli != nil {
-				s.removeClient(cli.UUID)
-			}
+				if conn := s.conns[path]; conn != nil {
+					conn.Close()
 
-			if conn != nil {
-				conn.Close()
-
-				// client disconnected
-				s.connsLock.Lock()
-				delete(s.conns, path)
-				s.connsLock.Unlock()
-			}
-		}
-	}()
-
-	go func() {
-		for {
-			var err error
-
-			<-ccPortOpened
-
-			if conn, err = dial(path); err != nil {
-				log.Error("dialing serial port %v failed: %v", path, err)
-				continue
-			}
-
-			cli, err = s.handshake(conn)
-			if err != nil {
-				if err != io.EOF {
-					// supress these, VM was probably never started
-					log.Error("handshake failed: %v", err)
+					s.connsLock.Lock()
+					delete(s.conns, path)
+					s.connsLock.Unlock()
 				}
 
+				return
+			}
+
+			conn, err := dial(path)
+			if err != nil {
+				log.Error("dialing serial port %v failed: %v", path, err)
+				return
+			}
+
+			cli, err := s.handshake(conn)
+			if err != nil {
+				log.Error("handshake failed (due to %v) - retrying", err)
+
+				time.Sleep(CLIENT_RECONNECT_RATE * time.Second)
+
 				continue
 			}
 
+			// This blocks, but will return on a loss of connection to the client.
 			s.clientHandler(cli)
+
+			log.Info("client handler for %s completed", path)
 		}
 	}()
 
@@ -655,11 +646,12 @@ func (s *Server) handshake(conn net.Conn) (*client, error) {
 	}
 
 	c := &client{
-		conn:        conn,
-		enc:         gob.NewEncoder(conn),
-		dec:         gob.NewDecoder(conn),
-		pipeReaders: make(map[string]*miniplumber.Reader),
-		pipeWriters: make(map[string]chan<- string),
+		conn:            conn,
+		enc:             gob.NewEncoder(conn),
+		dec:             gob.NewDecoder(conn),
+		pipeReaders:     make(map[string]*miniplumber.Reader),
+		pipeWriters:     make(map[string]chan<- string),
+		cancelHeartbeat: make(chan struct{}),
 	}
 
 	// get the first client struct as a handshake
@@ -694,7 +686,30 @@ func (s *Server) handshake(conn net.Conn) (*client, error) {
 
 	c.Namespace = namespace
 
-	if m.Client.Version != version.Revision {
+	if m.Client.Version == version.Revision {
+		log.Debug("miniccc version matches -- starting heartbeat to client")
+
+		// Only send heartbeats to client if versions match to prevent older clients
+		// from failing against this version of the server (sending a message to a
+		// client that doesn't recognize the message type will cause the client to
+		// fail).
+		go func() {
+			t := time.NewTicker(HEARTBEAT_RATE * time.Second)
+
+			for {
+				select {
+				case <-c.cancelHeartbeat:
+					log.Debug("stopping heartbeats to client %s", m.Client.UUID)
+					t.Stop()
+					return
+				case <-t.C:
+					log.Debug("sending HEARTBEAT to client %s", m.Client.UUID)
+					m := Message{Type: MESSAGE_HEARTBEAT}
+					c.enc.Encode(&m) // no need to worry about errors here
+				}
+			}
+		}()
+	} else {
 		log.Warn("mismatched miniccc version: %v", m.Client.Version)
 	}
 
@@ -853,6 +868,9 @@ func (s *Server) removeClient(uuid string) {
 		for _, p := range c.pipeWriters {
 			close(p)
 		}
+
+		// stop Goroutine sending heartbeats to this client
+		close(c.cancelHeartbeat)
 
 		delete(s.clients, uuid)
 	}
@@ -1152,6 +1170,7 @@ func (s *Server) clientReaper() {
 				log.Debug("client %v expired", k)
 				// the same as removeClient except we already hold clientLock
 				v.conn.Close()
+				close(v.cancelHeartbeat)
 				delete(s.clients, k)
 			}
 

commit 16952ae6c314a75f401216eff9fbda872256edf1

miniccc: fix map race conditions in ron server (#1461)
In commit dd04c33f introduced a bug in the ron server code that
handles serial cc connections -- failing to protect maps accessed
concurrently with their associated mutexes -- that would randomly
manifest itself into race conditions.

src/ron/server.go
@@ -230,16 +230,19 @@ func (s *Server) DialSerial(path, uuid string) error {
 	// completes if the VM still exists.
 	go func() {
 		for {
-			if _, ok := s.vms[uuid]; !ok {
+			s.clientLock.Lock()
+			_, exists := s.vms[uuid]
+			s.clientLock.Unlock()
+
+			if !exists {
 				log.Debug("vm %s (serial://%s) no longer exists -- closing serial connection", uuid, path)
 
+				s.connsLock.Lock()
 				if conn := s.conns[path]; conn != nil {
 					conn.Close()
-
-					s.connsLock.Lock()
 					delete(s.conns, path)
-					s.connsLock.Unlock()
 				}
+				s.connsLock.Unlock()
 
 				return
 			}