8e12532842eaaf7790416392151b21b9f4187b8a
jacdavi <86626873+jacdavi@users.noreply.github.com>
2023-12-14 22:50:21 +0000

    update year on copyright banners

--- pkg/minicli/output_test.go
+++ pkg/minicli/output_test.go
@@ -1,4 +1,4 @@
-// Copyright 2015-2021 National Technology & Engineering Solutions of Sandia, LLC (NTESS).
+// Copyright 2015-2023 National Technology & Engineering Solutions of Sandia, LLC (NTESS).
 // Under the terms of Contract DE-NA0003525 with NTESS, the U.S. Government retains certain
 // rights in this software.
 
