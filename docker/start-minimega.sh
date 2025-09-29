b59612b5dea71cd76ecfaa9d70481ebdb4e0d0cf
glattercj <glattercj@gmail.com>
2025-09-29 13:55:56 -0600

    feat: better docker logging and error handling

--- docker/start-minimega.sh
+++ docker/start-minimega.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 
-/usr/share/openvswitch/scripts/ovs-ctl start
+set -o pipefail
 
+# Check if there are values in /etc/default/minimega
+#   The order of precedence is:
+#     1. Existing environment variables
+#     2. Variables in /etc/default/minimega
+#     3. A set of defaults in this script
+if [[ -f "/etc/default/minimega" ]]; then
+    # Check if any variables are already set
+    while IFS='=' read -r key value; do
+        # Skip empty lines and comments
+        if [[ -n "$key" && -n "$value" && "$key" != \#* ]]; then
+            # Remove surrounding quotes
+            value="${value%\"}"
+            value="${value#\"}"
+
+            # Only set the variable if it is not already set
+            if [[ -z "${!key}" ]]; then
+                export "${key}=${value}"
+            fi
+        fi
+    done < <(grep -v '^#' "/etc/default/minimega")
+fi
+
+# Final default assignment (if these are not set already)
 : "${MINIWEB_ROOT:=/opt/minimega/web}"
 : "${MINIWEB_HOST:=0.0.0.0}"
 : "${MINIWEB_PORT:=9001}"
@@ -11,7 +34,7 @@
 : "${MM_BROADCAST:=255.255.255.255}"
 : "${MM_VLANRANGE:=101-4096}"
 : "${MM_PORT:=9000}"
-: "${MM_DEGREE:=2}"
+: "${MM_DEGREE:=1}"
 : "${MM_CONTEXT:=minimega}"
 : "${MM_LOGLEVEL:=info}"
 : "${MM_LOGFILE:=/var/log/minimega.log}"
@@ -20,48 +43,76 @@
 : "${MM_CGROUP:=/sys/fs/cgroup}"
 : "${MM_APPEND:=}"
 
+: "${OVS_APPEND:=}"
 : "${OVS_HOST_IFACE:=}"
 
-[[ -f "/etc/default/minimega" ]] && source "/etc/default/minimega"
-
-# Use OVS_HOST_IFACE env variable to auto add a host Ethernet interface(s) to an
-# OVS bridge. Note that the OVS bridge to add the interface(s) to must be
-# specified. The format of the value is "<bridge>:<port>[,<port>,...]".
-#
-# Single Interface Example (where bridge name is "phenix"): OVS_HOST_IFACE=phenix:eth0
-# Multi Interface Example (where bridge name is "phenix"): OVS_HOST_IFACE=phenix:eth0,eth1,eth2
-
-if [[ -v "OVS_HOST_IFACE" ]]; then
-  iface=(${OVS_HOST_IFACE//:/ })
-
-  if [[ -n "${iface[0]}" ]]; then
-    /usr/bin/ovs-vsctl --may-exist add-br ${iface[0]}
-    ip link set dev ${iface[0]} up
-  fi
-
-  if [[ -n "${iface[1]}" ]]; then
-    ports=(${iface[1]//,/ })
-
-    for port in "${ports[@]}"; do
-      /usr/bin/ovs-vsctl --may-exist add-port ${iface[0]} ${port}
-    done
-  fi
+# Start Open vSwitch
+/usr/share/openvswitch/scripts/ovs-ctl start ${OVS_APPEND} |& tee -a ${MM_LOGFILE}
+if [ ${PIPESTATUS[0]} -ne 0 ]; then
+    echo "failed to start Open vSwitch" | tee -a ${MM_LOGFILE}
+    exit 1
 fi
 
-/opt/minimega/bin/miniweb -root=${MINIWEB_ROOT} -addr=${MINIWEB_HOST}:${MINIWEB_PORT} &
+# Ensure Open vSwitch is available
+TIMEOUT=30
+INTERVAL=1
+START=$(date +%s)
 
+echo "waiting for Open vSwitch to become available (timeout: ${TIMEOUT}s)..." | tee -a ${MM_LOGFILE}
+while true; do
+    current=$(date +%s)
+    elapsed=$((current - START))
+
+    if [[ "$elapsed" -ge "$TIMEOUT" ]]; then
+        echo "failed to connect to Open vSwitch" | tee -a ${MM_LOGFILE}
+        exit 1
+    fi
+
+    if ovs-vsctl show &>/dev/null; then
+        break
+    else
+        sleep $INTERVAL
+    fi
+done
+
+# Check if there are bridge:port values to add
+if [[ -v "OVS_HOST_IFACE" ]]; then
+    iface=(${OVS_HOST_IFACE//:/ })
+    bridge=${iface[0]}
+
+    if [[ -n "${bridge}" ]]; then
+        echo -e "\tadding '${bridge}' bridge..." | tee -a ${MM_LOGFILE}
+        /usr/bin/ovs-vsctl --may-exist add-br ${bridge}
+        ip link set dev ${bridge} up
+    fi
+
+    if [[ -n "${iface[1]}" ]]; then
+        ports=(${iface[1]//,/ })
+
+        for port in "${ports[@]}"; do
+            echo -e "\tadding '${port}' port to '${bridge}' bridge..." | tee -a ${MM_LOGFILE}
+            /usr/bin/ovs-vsctl --may-exist add-port ${bridge} ${port}
+        done
+    fi
+fi
+
+echo "starting miniweb..." | tee -a ${MM_LOGFILE}
+/opt/minimega/bin/miniweb -root=${MINIWEB_ROOT} -addr=${MINIWEB_HOST}:${MINIWEB_PORT} &
+echo "miniweb started on ${MINIWEB_HOST}:${MINIWEB_PORT}" | tee -a ${MM_LOGFILE}
+
+echo "starting minimega..." | tee -a ${MM_LOGFILE}
 /opt/minimega/bin/minimega \
-  -nostdin \
-  -force=${MM_FORCE} \
-  -recover=${MM_RECOVER} \
-  -base=${MM_BASE} \
-  -filepath=${MM_FILEPATH} \
-  -broadcast=${MM_BROADCAST} \
-  -vlanrange=${MM_VLANRANGE} \
-  -port=${MM_PORT} \
-  -degree=${MM_DEGREE} \
-  -context=${MM_CONTEXT} \
-  -level=${MM_LOGLEVEL} \
-  -logfile=${MM_LOGFILE} \
-  -cgroup=${MM_CGROUP} \
-  ${MM_APPEND}
+    -nostdin \
+    -force=${MM_FORCE} \
+    -recover=${MM_RECOVER} \
+    -base=${MM_BASE} \
+    -filepath=${MM_FILEPATH} \
+    -broadcast=${MM_BROADCAST} \
+    -vlanrange=${MM_VLANRANGE} \
+    -port=${MM_PORT} \
+    -degree=${MM_DEGREE} \
+    -context=${MM_CONTEXT} \
+    -level=${MM_LOGLEVEL} \
+    -logfile=${MM_LOGFILE} \
+    -cgroup=${MM_CGROUP} \
+    ${MM_APPEND}
